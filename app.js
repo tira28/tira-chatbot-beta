553cfe090d6296cb2523e7dc059c49751fab3edd
Yudistira Wachyar <YWachyar@nl.ibm.com>
2016-12-01 10:23:55 +0100

    Change to weather API to the weather company

--- app.js
+++ app.js
@@ -222,18 +222,35 @@ function updateResponse(res, data) {
               lat: response.data.results[0].geometry.location.lat,
               lng: response.data.results[0].geometry.location.lng
           };
-          const DARK_SKY_API = process.env.DARKSKY_API_KEY;
-          var url_weather = `https://api.darksky.net/forecast/${DARK_SKY_API}/${location.lat},${location.lng}`;
+          // Dark Sky Credentials and URL
+          /*const DARK_SKY_API = process.env.DARKSKY_API_KEY;
+          var url_weather_dark_sky = `https://api.darksky.net/forecast/${DARK_SKY_API}/${location.lat},${location.lng}`;*/
+
+          // The Weather Company Credentials and URL
+          const WEATHER_COMPANY_USER = process.env.WEATHER_COMPANY_USERNAME;
+          const WEATHER_COMPANY_PASS = process.env.WEATHER_COMPANY_PASSWORD;
+          const WEATHER_COMPANY_PORT = process.env.WEATHER_COMPANY_PORT;
+
+          var url_weather_company = `https://${WEATHER_COMPANY_USER}:${WEATHER_COMPANY_PASS}@twcservice.eu-gb.mybluemix.net:${WEATHER_COMPANY_PORT}/api/weather` + `/v1/geocode/${location.lat}/${location.lng}/forecast/daily/3day.json`;
+
 
           // call dark sky API to retrieve weather data
-          return axios.get(url_weather);
+          return axios.get(url_weather_company);
       }).then(function(response){
-          var summary = response.data.currently.summary;
-          var apparentTemperature = response.data.currently.apparentTemperature;
+          console.log(response.data.forecasts[0].day.temp);
+          console.log(response.data.forecasts[0].day.shortcast);
+
+          // Summary for The Weather Company
+          var summary = response.data.forecasts[0].day.shortcast;
+          var apparentTemperature = response.data.forecasts[0].day.temp;
+
+          // Summary for Dark Sky API
+          /*var summary = response.data.currently.summary;
+          var apparentTemperature = response.data.currently.apparentTemperature;*/
           var city = data.context.appCity;
 
           // make watson returns weather prediction in selected city
-          data.output.text = `${summary} in ${city} with temperature ${(apparentTemperature - 32).toFixed(2)} C.`;
+          data.output.text = `${summary} in ${city} with temperature ${(apparentTemperature - 32).toFixed(0)} C.`;
           return res.json(data);
       }).catch(function(error){
           console.log(error);
--- bot-facebook.js
+++ bot-facebook.js
@@ -60,15 +60,37 @@ controller.hears('(.*)', 'message_received', function(bot, message) {
                     lat: response.data.results[0].geometry.location.lat,
                     lng: response.data.results[0].geometry.location.lng
                 };
-                const DARK_SKY_API = process.env.DARKSKY_API_KEY;
-                console.log('lat:', location.lat);
-                console.log('lng', location.lng);
-                var url_weather = `https://api.darksky.net/forecast/${DARK_SKY_API}/${location.lat},${location.lng}`;
-                return axios.get(url_weather);
+
+                console.log('lat:', location.lat.toFixed(2));
+                console.log('lng', location.lng.toFixed(2));
+
+                // Dark sky API
+                /*const DARK_SKY_API = process.env.DARKSKY_API_KEY;
+                var url_weather = `https://api.darksky.net/forecast/${DARK_SKY_API}/${location.lat},${location.lng}`;*/
+
+                // The weather company API
+                // The Weather Company Credentials and URL
+                const WEATHER_COMPANY_USER = process.env.WEATHER_COMPANY_USERNAME;
+                const WEATHER_COMPANY_PASS = process.env.WEATHER_COMPANY_PASSWORD;
+                const WEATHER_COMPANY_PORT = process.env.WEATHER_COMPANY_PORT;
+
+                var url_weather_company = `https://${WEATHER_COMPANY_USER}:${WEATHER_COMPANY_PASS}@twcservice.eu-gb.mybluemix.net:${WEATHER_COMPANY_PORT}/api/weather` + `/v1/geocode/${location.lat}/${location.lng}/forecast/daily/3day.json`;
+
+
+                return axios.get(url_weather_company);
             }).then(function(response){
-                var summary = response.data.currently.summary;
-                var apparentTemperature = response.data.currently.apparentTemperature;
-                output.text = `${summary} in ${city} with temperature ${(apparentTemperature - 32).toFixed(2)} C.`;
+
+                console.log(response.data.forecasts[0].day.temp);
+                console.log(response.data.forecasts[0].day.shortcast);
+
+                // Results from dark weather API
+                /*var summary = response.data.currently.summary;
+                var apparentTemperature = response.data.currently.apparentTemperature;*/
+
+                var summary = response.data.forecasts[0].day.shortcast;
+                var apparentTemperature = response.data.forecasts[0].day.temp;
+
+                output.text = `${summary} in ${city} with temperature ${(apparentTemperature - 32).toFixed(0)} C.`;
             }).then(function(){
                 console.log(output.text);
                 return bot.reply(message, output.text);
@@ -116,6 +138,18 @@ controller.hears('(.*)', 'message_received', function(bot, message) {
             });
 
             break;
+
+        case 'book_ticket':
+
+
+            console.log(watsonMessage);
+            console.log(context.origin_airport);
+            console.log(context.destination_airport);
+            console.log(context.departure_date);
+            console.log(context.origin_departing_time);
+            console.log(context.return_date);
+            console.log(context.returning_departing_time);
+
 
         default:
             output.text;
